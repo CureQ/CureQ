3193e4ef5f36bef552f86c8eb90a3085c36e989a
Jesse <jesse.antonissen@gmail.com>
2026-01-14 15:17:28 +0100

    Working MEAlytics application (Undid last commits for the UvA master course CCB)

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 # Setup metadata for initializing the library
 setuptools.setup(
-    name="CureQ",
-    version="1.2.15",
+    name="cureq",
+    version="1.2.18",
     author="CureQ",
     author_email="cureq-ft@hva.nl",
     description="Library for analyzing MEA files.",
